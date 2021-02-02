de8e76580c801f1ea3a88166d4f01af50cf7ea53
Ashton Moore <ashton.chris.moore@gmail.com>
2021-02-02 21:17:17 +1300

    Updates to example config

--- config.example.py
+++ config.example.py
@@ -1,9 +1,14 @@
 # Pro tip: create you own config.py
 # by using this as a template
 
+# Don't put credentials in config files!
+# Set them as environment variables in your shell
+
+import os
+
 ''' ========= LOGIN =========== '''
-username = 'your.email@mail.com'
-password = 'password123'
+username = os.environ["email_shell_variable"]
+password = os.environ["password_shell_variable"]
 
 ''' ======== BUY/SELL ========= '''
 buy_amount          = 10    # Dollar amount to buy when a good stock is found.
@@ -12,7 +17,7 @@ sell_profit_margin  = 2     # Profit percentage to reach before selling a stock.
 dividends_bonus     = 2     # Multiplier for stocks with an upcoming dividend (ignored when <=1).
 
 ''' ========= TRADING ========= '''
-scan_interval   = 30
-open_time       = '9:00am'
-close_time      = '4:45pm'
+scan_interval   = 30        # In minutes
+open_time       = '10:00am'
+close_time      = '5:00pm'
 days_closed     = ['saturday', 'sunday']
